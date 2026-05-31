bd2965d5804f36f1e5dd5adf3f7399dea5ac77b3
Preclik <ransdorfd@gmail.com>
2026-05-31 19:00:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,17 +8,9 @@ SERVER_PID=$!
 
 echo "Starting notify service..."
 
-(
-  cd internal/services/
-  chmod +x ./notify
-  ./notify
-) &
-NOTIFY_PID=$!
-
 echo "Server PID: $SERVER_PID"
-echo "Notify PID: $NOTIFY_PID"
 
-echo "Both services are running in background."
+echo "Services are running in background."
 echo "Terminal is free to use."
 
 # Optional: keep script alive if you want
